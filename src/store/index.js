28dea9952d8ab29c70fe6c09a69d7a0be114f0cf
CodeEcho-Yao <1365560286@qq.com>
2022-08-18 09:57:00 +0800

    Login Register

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore } from 'vuex'
+import ModuleUser from './user'
 
 export default createStore({
-  state: {
+  state: {        // 存储所有数据，可以用modules属性划分成若干模块
   },
-  getters: {
+  getters: {      // 根据state中的值计算新的值
   },
-  mutations: {
+  mutations: {    // 所有对state的修改操作都需要定义在这里，不支持异步，可以通过$store.commit()触发
   },
-  actions: {
+  actions: {      // 定义对state的复杂修改操作, 支持异步, 可以通过$store.dispatch()触发 注意不能直接修改state, 只能通过mutations修改state
   },
-  modules: {
+  modules: {      // 定义state的子模块
+    user: ModuleUser
   }
 })
--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -18,6 +18,7 @@ import UserProfileInfo from '../components/UserProfileInfo';
 import UserProfilePosts from '../components/UserProfilePosts';
 import UserProfileWrite from '../components/UserProfileWrite';
 import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
   name: 'UserProfileView',
@@ -28,6 +29,10 @@ export default {
     UserProfileWrite
   },
   setup() {
+    const route = useRoute();
+    const userId = route.params.userId;
+    console.log(userId)
+
     const user = reactive({
       id: 1,
       username: "CodeEcho",
